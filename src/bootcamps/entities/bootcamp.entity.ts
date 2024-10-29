4bb6525ec0bb66e99a1c6482acd0e55eebda4d3b
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-10-29 10:51:37 -0500

    Ultimo taller-sin conexion

---
+++ src/bootcamps/entities/bootcamp.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Bootcamp {
+
+@PrimaryGeneratedColumn()
+id:number
+
+@Column()
+phone:number
+
+@Column ()
+name: string
+
+@Column({name : 'average_number'})
+averageRating: number
+
+}
